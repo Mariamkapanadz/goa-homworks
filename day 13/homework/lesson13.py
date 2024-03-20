e0286696939b437fba32450e241d9a371cb3fd3e
Mariami <kapanadze.a@gmail.com>
2024-03-20 19:21:57 +0400

    commiit

---
+++ day 13/homework/lesson13.py
@@ -0,0 +1,9 @@
+print("true and false")
+print("false and true")
+print("false and false")
+print("true and true")
+
+print("true and true")
+print("true and false")
+print("false and true")
+print("false and false")
